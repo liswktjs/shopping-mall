31a9c9dba1b73177082b44ea695393422aadea7f
JASUN LEE <liswktjs@gmail.com>
2023-03-14 14:55:50 +0900

    style: 전역 스타일 reset

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ThemeProvider } from '@emotion/react';
-
+import { Global, ThemeProvider } from '@emotion/react';
 import CustomRouter from './router/CustomRouter';
 import queryClient from './query/queryClient';
+
+import Header from './layout/Header/Header';
 import { theme } from './style/theme';
+import { reset } from './style/reset';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <Global styles={reset} />
         <ThemeProvider theme={theme}>
+          <Header />
           <CustomRouter />
         </ThemeProvider>
       </QueryClientProvider>
