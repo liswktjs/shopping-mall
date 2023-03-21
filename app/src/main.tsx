e1b49ef791c2b8e0484f50fd460efb0de5b0c8b5
JASUN LEE <liswktjs@gmail.com>
2023-03-21 13:14:18 +0900

    feat: Footer 적용

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Global, ThemeProvider } from '@emotion/react';
+
 import CustomRouter from './router/CustomRouter';
 import queryClient from './query/queryClient';
 
 import Header from './layout/Header/Header';
+import Footer from './layout/Footer/Footer';
+
 import { theme } from './style/theme';
 import { reset } from './style/reset';
 
@@ -18,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <ThemeProvider theme={theme}>
           <Header />
           <CustomRouter />
+          <Footer />
         </ThemeProvider>
       </QueryClientProvider>
     </BrowserRouter>
